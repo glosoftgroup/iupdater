a94fce04585695c4004dd4de5f0e5099319da118
kenneth.marete <maretekent@gmail.com>
2018-11-21 14:06:22 +0300

    stage the initial changes

---
+++ src/main/java/com/idealupdater/Main.java
@@ -0,0 +1,33 @@
+package com.idealupdater;
+
+import com.idealupdater.utils.structlog4j.GenericLoggableObject;
+import com.idealupdater.utils.structlog4j.LoggerFactory;
+import com.idealupdater.utils.structlog4j.StructLog4JConfig;
+import com.idealupdater.utils.structlog4j.interfaces.Logger;
+import com.idealupdater.utils.structlog4j.renderers.JSONRenderer;
+import com.idealupdater.utils.ui.SystemTrayUtils;
+
+
+public class Main {
+
+    public static final String VERSION = "1.0.0";
+    public static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static final String LOG_TAG = "Main";
+
+    public static void init(){
+        logger.bind("test", "test");
+        // initialise background service
+        logger.info(LOG_TAG, "event", "Initialize services");
+    }
+
+    public static void main(String[] args){
+//        StructLog4JConfig.setLogRenderer(new JSONRenderer());
+        StructLog4JConfig.setContextSupplier(
+                new GenericLoggableObject(new Object[]{"environment", "development"}));
+        init();
+        // show the TrayIcon with progress bar update
+        logger.info(LOG_TAG, "event", "Application started");
+
+        SystemTrayUtils.getInstance().viewTray();
+    }
+}
---
+++ src/main/java/com/idealupdater/utils/ui/SystemTrayUtils.java
@@ -0,0 +1,179 @@
+package com.idealupdater.utils.ui;
+
+import com.idealupdater.utils.structlog4j.LoggerFactory;
+import com.idealupdater.utils.structlog4j.interfaces.Logger;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import javax.swing.*;
+
+public class SystemTrayUtils {
+
+    private static SystemTrayUtils instance = null;
+    public static final Logger logger = LoggerFactory.getLogger(SystemTrayUtils.class);
+    public static final String LOG_TAG = "SystemTrayUtils";
+
+    private SystemTrayUtils() {}
+
+    public static SystemTrayUtils getInstance () {
+        if (instance == null) {
+            instance = new SystemTrayUtils();
+        }
+        return instance;
+    }
+
+    public void viewTray() {
+
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+        //Schedule a job for the event-dispatching thread:
+        //adding TrayIcon.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        //Check the SystemTray support
+        if (!SystemTray.isSupported()) {
+            logger.error(LOG_TAG, "event", "Not supported", "message", "SystemTray is not supported");
+            return;
+        }
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon =
+                new TrayIcon(createImage("etraybulb.gif", "tray icon"));
+        trayIcon.setImageAutoSize(true);
+        final SystemTray tray = SystemTray.getSystemTray();
+        // Create a popup menu components
+        MenuItem aboutItem = new MenuItem("About");
+        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
+        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+        Menu displayMenu = new Menu("Display");
+        MenuItem errorItem = new MenuItem("Error");
+        MenuItem warningItem = new MenuItem("Warning");
+        MenuItem infoItem = new MenuItem("Info");
+        MenuItem noneItem = new MenuItem("None");
+        MenuItem exitItem = new MenuItem("Exit");
+        //Add components to popup menu
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(cb1);
+        popup.add(cb2);
+        popup.addSeparator();
+        popup.add(displayMenu);
+        displayMenu.add(errorItem);
+        displayMenu.add(warningItem);
+        displayMenu.add(infoItem);
+        displayMenu.add(noneItem);
+        popup.add(exitItem);
+        trayIcon.setPopupMenu(popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+            return;
+        }
+        trayIcon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                        "This dialog box is run from System Tray");
+            }
+        });
+
+        aboutItem.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                        "This dialog box is run from the About menu item");
+            }
+        });
+
+        cb1.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                int cb1Id = e.getStateChange();
+                if (cb1Id == ItemEvent.SELECTED){
+                    trayIcon.setImageAutoSize(true);
+                } else {
+                    trayIcon.setImageAutoSize(false);
+                }
+            }
+        });
+
+        cb2.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                int cb2Id = e.getStateChange();
+                if (cb2Id == ItemEvent.SELECTED){
+                    trayIcon.setToolTip("Sun TrayIcon");
+                } else {
+                    trayIcon.setToolTip(null);
+                }
+            }
+        });
+
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                MenuItem item = (MenuItem)e.getSource();
+                //TrayIcon.MessageType type = null;
+                System.out.println(item.getLabel());
+                if ("Error".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.ERROR;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an error message", TrayIcon.MessageType.ERROR);
+                } else if ("Warning".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.WARNING;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is a warning message", TrayIcon.MessageType.WARNING);
+                } else if ("Info".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.INFO;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an info message", TrayIcon.MessageType.INFO);
+                } else if ("None".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.NONE;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an ordinary message", TrayIcon.MessageType.NONE);
+                }
+            }
+        };
+        errorItem.addActionListener(listener);
+        warningItem.addActionListener(listener);
+        infoItem.addActionListener(listener);
+        noneItem.addActionListener(listener);
+
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //stop services
+                tray.remove(trayIcon);
+                System.exit(0);
+            }
+        });
+
+
+    }
+
+    //Obtain the image URL
+    protected static Image createImage(String path, String description) {
+        URL imageURL = SystemTrayUtils.class.getClassLoader().getResource(path);
+        if (imageURL == null) {
+            String message = "Resource not found: " + path;
+            logger.error(LOG_TAG, "event", "Not suported", "message", message);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
+    }
+
+}
