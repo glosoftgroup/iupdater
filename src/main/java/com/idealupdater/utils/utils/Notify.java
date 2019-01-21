7e1e1751c80694c5f1b1458ed24c2498c87a7324
kiburualex <alexkiburu@gmail.com>
2019-01-21 18:46:40 +0300

    WHAT ---- 1. renamed the buttons of the material custom alert dialog 2. added an confirmation dialog 3. added a normal custom alert 4. added create input dialog 5. added a getter and setter for open status of the dialog 6. added read exec statements for executing commands during the update process

--- src/main/java/com/idealupdater/utils/utils/Notify.java
+++ src/main/java/com/idealupdater/utils/utils/Notify.java
@@ -1,18 +1,28 @@
 package com.idealupdater.utils.utils;
 
+import com.jfoenix.controls.JFXAlert;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class Notify {
+    public StackPane stackPane;
     public boolean status = false;
 
     public void showSuccess(StackPane rootAnchorPane, String title, String message){
@@ -33,39 +43,39 @@ public class Notify {
         headerLbl.setPrefWidth(330);
 
         // the close button on the header
-        JFXButton headerBtn = new JFXButton();
-        headerBtn.setPadding(new Insets(8, 16, 8, 17));
-        headerBtn.getStyleClass().add("close-icon");
+        JFXButton closeIconBtn = new JFXButton();
+        closeIconBtn.setPadding(new Insets(8, 16, 8, 17));
+        closeIconBtn.getStyleClass().add("close-icon");
         // add the close icon to the button
         FontAwesomeIconView icon = new FontAwesomeIconView();
         icon.setIcon(FontAwesomeIcon.TIMES);
         icon.setSize("17px");
         icon.setStyle("-fx-fill: white;");
-        headerBtn.setGraphic(icon);
+        closeIconBtn.setGraphic(icon);
 
         HBox hbox = new HBox();
         hbox.setPrefWidth(content.getPrefWidth());
         hbox.getChildren().add(headerLbl);
-        hbox.getChildren().add(headerBtn);
+        hbox.getChildren().add(closeIconBtn);
 
         content.setHeading(hbox);
         content.setBody(new Label(message));
 
         JFXDialog dialog = new JFXDialog(rootAnchorPane, content, JFXDialog.DialogTransition.CENTER);
-        JFXButton button = new JFXButton("CLOSE");
-        button.getStyleClass().add("dialog-button");
-        Label lbl = new Label();
-        lbl.setPrefWidth(content.getPrefWidth());
-        lbl.setPrefWidth(140);
+        JFXButton closeBtn = new JFXButton("CLOSE");
+        closeBtn.getStyleClass().add("dialog-button");
+        Label spaceLabel = new Label();
+        spaceLabel.setPrefWidth(content.getPrefWidth());
+        spaceLabel.setPrefWidth(140);
 
-        headerBtn.setOnAction(new EventHandler<ActionEvent>() {
+        closeIconBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 dialog.close();
             }
         });
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
+        closeBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 dialog.close();
@@ -75,9 +85,79 @@ public class Notify {
             System.out.println("I am closed!");
         });
 
-        content.setActions(button, lbl);
+        content.setActions(closeBtn, spaceLabel);
         content.getStylesheets().add("/styles/alerts/"+type+".css");
         dialog.setOverlayClose(false);
         dialog.show();
+    }
+
+
+
+    public boolean AlertConfirm(StackPane stackPane, String msg, String Title){
+        JFXAlert<Boolean> alert = new JFXAlert<>((Stage) stackPane.getScene().getWindow());
+        JFXDialogLayout content = new JFXDialogLayout();
+        content.setHeading(new Label(Title));
+        content.setBody(new Label(msg));
+
+        content.getStylesheets().add("/styles/alerts/info.css");
+
+        JFXButton closeButton = new JFXButton("CLOSE");
+        closeButton.getStyleClass().add("close-button");
+
+        JFXButton yesButton = new JFXButton("YES");
+        yesButton.getStyleClass().add("dialog-button");
+
+        closeButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event)->{
+            status = false;
+            alert.close();
+        });
+        yesButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event)->{
+            status = true;
+            alert.close();
+        });
+
+        content.setActions(closeButton, yesButton);
+        alert.setContent(content);
+        alert.showAndWait();
+        return status;
+    }
+
+    public void CreateAlert(String msg, String Title,Alert.AlertType type){
+        Platform.runLater(()->{
+            Alert alert=new Alert(type);
+            alert.setHeaderText(msg);
+            alert.setTitle(Title);
+            alert.showAndWait();
+        });
+    }
+
+    public boolean CreateConfirmDialog(String msg, String Title){
+        Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(Title);
+        alert.setHeaderText(msg);
+        Optional<ButtonType> results=alert.showAndWait();
+        if(results.get()==ButtonType.OK){
+            return true;
+        }
+        return false;
+    }
+
+    public int CreateInputDialogue(){
+        TextInputDialog dialog = new TextInputDialog("1");
+        dialog.setTitle("Change Quantity");
+        dialog.setHeaderText("Change product Quantity");
+        dialog.setContentText("Enter new Value:");
+        dialog.getEditor().setOnKeyTyped(e->{
+            if(!e.getCharacter().matches("[0-9]")){
+                e.consume();
+            }
+        });
+        Optional<String> result = dialog.showAndWait();
+
+        if(result.isPresent())
+            return Integer.parseInt(result.get());
+
+        return 0;
+
     }
 }
